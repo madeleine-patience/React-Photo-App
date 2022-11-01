5a4d4440a60cb254374bcad263bb8dc2bd83be5f
Madeleine Belanger <madeleinepbelanger@gmail.com>
2022-10-31 20:57:25 -0700

    All Files

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title, onAdd, showAdd}) => {
+
+  return (
+    <header className= 'header'>
+        <h1 > My Photos </h1>
+        <Button color={showAdd? 'red': 'green'} text={showAdd? "Close" : "Add"} onClick={onAdd}/>
+     
+
+    </header>
+  )
+}
+
+Header.defaultProps={
+    title: 'Task Tracker'
+
+}
+
+
+Header.propTypes ={
+    title: PropTypes.string,
+
+
+}
+
+
+export default Header
